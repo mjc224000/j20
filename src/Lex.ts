7230892418a6c3910f20425ec2925163697f330d
mjc224000 <397938635@qq.com>
2022-09-16 17:19:23 +0800

    m

--- src/Lex.ts
+++ src/Lex.ts
@@ -1,24 +1,46 @@
-import {Token,Float,Integer,Literal} from "./token";
+import {Float, Literal, Token} from "./token";
 
 const idPreReg = /[a-zA-Z_$]/;
+const idNumReg = /\d/;
 
 export class Lex {
+    tokens: [] = []
+
     constructor(str: string) {
         let i = 0;
-        const tokens:Token[]=[];
+        const tokens: Token[] = [];
         while (i < str.length) {
-            const cur = str[i];
-            let text='';
-            if (idPreReg.test(cur)) {
+            let text = '';
+            if (idPreReg.test(str[i])) {
+                text += str[i];
+                while (/\w/.test(str[++i])) {
+                    text += str[i];
+                }
+                tokens.push(new Literal(text));
+            }
+            if (idNumReg.test(str[i])) {
+                text = str[i];
+                while (idNumReg.test(str[++i])) {
+                    text += str[i];
+                }
+                if (str[i] === '.') {
+                    text += str[i];
+                    while (idNumReg.test(str[++i])) {
+                        text += str[i];
+                    }
+                    tokens.push(new Float(text));
+                }
 
             }
+
             i++;
         }
+
     }
 }
 
-for (let i = 0; i <10 ; i++) {
-    function foo(){
-            console.log(i);
+for (let i = 0; i < 10; i++) {
+    function foo() {
+        console.log(i);
     }
 }
--- src/token.ts
+++ src/token.ts
@@ -1,15 +1,23 @@
 export class Token {
+    text
+
+    constructor(text) {
+        this.text = text;
+    }
 }
 
 export class Integer extends Token {
 
 }
 
-export class Float extends Token{
+export class String extends Token {
+}
+
+export class Float extends Token {
 
 }
 
-export class Literal extends Token{
+export class Literal extends Token {
 
 }
 
